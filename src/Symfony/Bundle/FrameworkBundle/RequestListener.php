9efb29db72ff2564de417b0e25a7f927acb82588
Francis Besset <francis.besset@free.fr>
2010-11-07 23:10:41 +0100

    Add routing internationalization
    [FrameworkBundle]
    Add routing internationalization
    In your routing.yml:
    homepage:
        pattern:  /:_locale
        defaults: { _controller: MyBundle:MyController:index, _locale: en }
        requirements: { _locale: (en|fr) }
    
    In your PHP template:
    <?php echo $view['session']->getLocale() ?>

--- src/Symfony/Bundle/FrameworkBundle/RequestListener.php
+++ src/Symfony/Bundle/FrameworkBundle/RequestListener.php
@@ -68,6 +68,12 @@ class RequestListener
             }
 
             $request->attributes->add($parameters);
+
+            $session = $request->getSession();
+            if ($request->attributes->get('_locale') && $request->attributes->get('_locale') != $session->getLocale()) {
+                $session->setLocale($request->attributes->get('_locale'));
+                $session->save();
+            }
         } elseif (null !== $this->logger) {
             $this->logger->err(sprintf('No route found for %s', $request->getPathInfo()));
         }
