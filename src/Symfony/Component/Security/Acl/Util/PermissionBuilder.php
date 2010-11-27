eada9c33879321287b47dc27e86d7e2a7d01a1cc
Johannes Schmitt <schmittjoh@gmail.com>
2010-11-27 14:27:51 +0100

    moved formatting to the PermissionBuilder, and removed unused class

--- src/Symfony/Component/Security/Acl/Util/PermissionBuilder.php
+++ src/Symfony/Component/Security/Acl/Util/PermissionBuilder.php
@@ -2,6 +2,23 @@
 
 namespace Symfony\Component\Security\Acl\Util;
 
+/**
+ * This class allows you to build cumulative permissions easily, or convert
+ * masks to a human-readable format.
+ * 
+ * <code>
+ *   	$builder = new PermissionBuilder();
+ *   	$builder
+ *   		->add('view')
+ *   		->add('create')
+ *   		->add('edit')
+ *   	;
+ *   	var_dump($builder->get()); // int(7)
+ *   	var_dump($builder->pattern()); // string(32) ".............................ECV"
+ * </code>
+ * 
+ * @author Johannes M. Schmitt <schmittjoh@gmail.com>
+ */
 class PermissionBuilder
 {
     const MASK_VIEW         = 1;      // 1 << 0
@@ -12,11 +29,27 @@ class PermissionBuilder
     const MASK_ADMINISTER   = 32;     // 1 << 5
     const MASK_OWNER        = 64;     // 1 << 6
     
+    const CODE_VIEW         = 'V';
+    const CODE_CREATE       = 'C';
+    const CODE_EDIT         = 'E';
+    const CODE_DELETE       = 'D';
+    const CODE_UNDELETE     = 'U';
+    const CODE_ADMINISTER   = 'A';
+    const CODE_OWNER        = 'O';
+    
+    const ALL_OFF           = '................................';
+    const OFF               = '.';
+    const ON                = '*';
+    
     protected $mask;
     
-    public function __construct()
+    public function __construct($mask = 0)
     {
-        $this->mask = 0;
+        if (!is_int($mask)) {
+            throw new \InvalidArgumentException('$mask must be an integer.');
+        }
+        
+        $this->mask = $mask;
     }
     
     public function add($mask)
@@ -47,6 +80,26 @@ class PermissionBuilder
         return $this;
     }
     
+    public function pattern()
+    {
+        $pattern = self::ALL_OFF;
+        $length = strlen($pattern);
+        $bitmask = str_pad(decbin($this->mask), $length, '0', STR_PAD_LEFT);
+        
+        for ($i=$length-1; $i>=0; $i--) {
+            if ('1' === $bitmask[$i]) {
+                try {
+                    $pattern[$i] = self::getCode(1 << ($length - $i));
+                }
+                catch (\Exception $notPredefined) {
+                    $pattern[$i] = self::ON;
+                }
+            }
+        }
+        
+        return $pattern;
+    }
+    
     public function get()
     {
         return $this->mask;
@@ -57,5 +110,29 @@ class PermissionBuilder
         $this->mask = 0;
         
         return $this;
+    }
+    
+    public static function getCode($mask)
+    {
+        if (!is_int($mask)) {
+            throw new \InvalidArgumentException('$mask must be an integer.');
+        }
+        
+        $reflection = new \ReflectionClass(get_called_class());
+        foreach ($reflection->getConstants() as $name => $cMask) {
+            if (false === strpos($name, 'MASK_')) {
+                continue;
+            }
+            
+            if ($mask === $cMask) {
+                if (!defined($cName = 'self::CODE_'.substr($name, 5))) {
+                    throw new \RuntimeException('There was no code defined for this mask.');
+                }
+                
+                return constant($cName);
+            }
+        }
+        
+        throw new \InvalidArgumentException(sprintf('The mask "%d" is not supported.', $mask));
     }
 }
