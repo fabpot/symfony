d6c938d02f72dd881d1da61fea73a2f3e4159a31
Ryan Weaver <ryan@thatsquality.com>
2010-12-03 09:51:21 -0600

    [HttpKernel] Reworking the HttpException class constructor to be more consistent with normal OO classes. Additionally, the base HttpException constructor was changed to require a code argument as it doesn't make sense to create an exception that will translate into a status code of 0 (in fact it'll cause a strange error).
    Also added a FlattenException::create() test.

--- src/Symfony/Component/HttpKernel/Exception/ForbiddenHttpException.php
+++ src/Symfony/Component/HttpKernel/Exception/ForbiddenHttpException.php
@@ -18,12 +18,12 @@ namespace Symfony\Component\HttpKernel\Exception;
  */
 class ForbiddenHttpException extends HttpException
 {
-    public function __construct($message = '', $code = 0, \Exception $previous = null)
+    public function __construct($message = '', $code = 403, \Exception $previous = null)
     {
         if (!$message) {
             $message = 'Forbidden';
         }
 
-        parent::__construct($message, 403, $previous);
+        parent::__construct($message, $code, $previous);
     }
 }
--- src/Symfony/Component/HttpKernel/Exception/UnauthorizedHttpException.php
+++ src/Symfony/Component/HttpKernel/Exception/UnauthorizedHttpException.php
@@ -18,12 +18,12 @@ namespace Symfony\Component\HttpKernel\Exception;
  */
 class UnauthorizedHttpException extends HttpException
 {
-    public function __construct($message = '', $code = 0, \Exception $previous = null)
+    public function __construct($message = '', $code = 401, \Exception $previous = null)
     {
         if (!$message) {
             $message = 'Unauthorized';
         }
 
-        parent::__construct($message, 401, $previous);
+        parent::__construct($message, $code, $previous);
     }
 }
