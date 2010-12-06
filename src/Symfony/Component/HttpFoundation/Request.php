e3eafd2f3fdca045af4b21dd6c2902abaf0a2a3c
Jordi Boggiano <j.boggiano@seld.be>
2010-12-06 01:34:54 +0100

    [HttpFoundation] Always force the request method to full caps

--- src/Symfony/Component/HttpFoundation/Request.php
+++ src/Symfony/Component/HttpFoundation/Request.php
@@ -129,7 +129,7 @@ class Request
      *
      * @return Request A Request instance
      */
-    static public function create($uri, $method = 'get', $parameters = array(), $cookies = array(), $files = array(), $server = array())
+    static public function create($uri, $method = 'GET', $parameters = array(), $cookies = array(), $files = array(), $server = array())
     {
         $defaults = array(
             'SERVER_NAME'          => 'localhost',
@@ -500,8 +500,8 @@ class Request
 
     public function setMethod($method)
     {
-        $this->method = null;
-        $this->server->set('REQUEST_METHOD', 'GET');
+        $this->method = strtoupper($method);
+        $this->server->set('REQUEST_METHOD', $this->method);
     }
 
     /**
@@ -617,7 +617,7 @@ class Request
 
     public function isMethodSafe()
     {
-        return in_array(strtolower($this->getMethod()), array('get', 'head'));
+        return in_array($this->getMethod(), array('GET', 'HEAD'));
     }
 
     public function getETags()
