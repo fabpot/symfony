b5584d98ffd91d30d10da53602709d467d92fc91
Bulat Shakirzyanov <mallluhuct@gmail.com>
2011-01-26 16:40:44 -0500

    [HttpFoundation] initialize request parameters to empty arrays

--- src/Symfony/Component/HttpFoundation/Request.php
+++ src/Symfony/Component/HttpFoundation/Request.php
@@ -80,7 +80,7 @@ class Request
      * @param array $files      The FILES parameters
      * @param array $server     The SERVER parameters
      */
-    public function __construct(array $query = null, array $request = null, array $attributes = null, array $cookies = null, array $files = null, array $server = null)
+    public function __construct(array $query = array(), array $request = array(), array $attributes = array(), array $cookies = array(), array $files = array(), array $server = array())
     {
         $this->initialize($query, $request, $attributes, $cookies, $files, $server);
     }
@@ -97,14 +97,14 @@ class Request
      * @param array $files      The FILES parameters
      * @param array $server     The SERVER parameters
      */
-    public function initialize(array $query = null, array $request = null, array $attributes = null, array $cookies = null, array $files = null, array $server = null)
+    public function initialize(array $query = array(), array $request = array(), array $attributes = array(), array $cookies = array(), array $files = array(), array $server = array())
     {
-        $this->request = new ParameterBag(null !== $request ? $request : $_POST);
-        $this->query = new ParameterBag(null !== $query ? $query : $_GET);
-        $this->attributes = new ParameterBag(null !== $attributes ? $attributes : array());
-        $this->cookies = new ParameterBag(null !== $cookies ? $cookies : $_COOKIE);
-        $this->files = new FileBag(null !== $files ? $files : $_FILES);
-        $this->server = new ServerBag(null !== $server ? $server : $_SERVER);
+        $this->request = new ParameterBag($request ? $request : $_POST);
+        $this->query = new ParameterBag($query ? $query : $_GET);
+        $this->attributes = new ParameterBag($attributes ? $attributes : array());
+        $this->cookies = new ParameterBag($cookies ? $cookies : $_COOKIE);
+        $this->files = new FileBag($files ? $files : $_FILES);
+        $this->server = new ServerBag($server ? $server : $_SERVER);
         $this->headers = new HeaderBag($this->server->getHeaders());
 
         $this->content = null;
